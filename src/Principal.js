c431251f30bf72a3b22bdcd2c940e674aa64272e
AndersonClayton <andersonclaytondesenvolvedor@gmail.com>
2022-11-16 15:00:34 -0300

    Componente reusável concluído

--- src/Principal.js
+++ src/Principal.js
@@ -1,17 +1,37 @@
 import './App.css';
-import {daggers, swords} from './Database';
+import { swords } from './Database';
+import React from "react";
 
 function Principal() {
-    const vetor = ["Shop: "];
+    return Reusa(swords, "Swords");
+}
+
+function Reusa(conjunto, tipo) {
     return(
         <div className="Container-equipamento">
-            <div className="Tipo-equipamento">
-                <div className="Equipamento">Weapon</div>
-                <div className="Descricao">Como adquirir</div>
-                <div className='Equipamento' >{swords[0].Nome}</div>
-                <div className="Descricao">{vetor[0]}</div>
-            </div>
-        </div>
+        <h1>{tipo}</h1>
+        <section>
+            <header className='cabecalho'>
+                <div className='col'>Equipamento</div>
+                <div className='col'>Como adquirir</div>
+            </header>
+            {conjunto.map((element, index0) => {
+                return(
+                    <div key={index0} className='row'>
+                        <div className='col'>
+                            <span className='destaque nomeador'>{element.Nome}</span>
+                        </div>
+                        <div className='col'>
+                            {Object.keys(element).map((espada, index) => {
+                                return element[espada] !== "" ? 
+                                index !== 0 ? <div key={index}><span className='destaque'>{espada}:</span> {element[espada]}</div> : "" : "";
+                            })}
+                        </div>
+                    </div>
+                )
+            })}
+        </section>
+    </div>
     );
 }
 
